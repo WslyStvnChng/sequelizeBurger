105c3e12c82b299965fb7a8fbcca85309cd70e0f
WslyStvnChng <mr.cheng.st@gmail.com^C>
2018-03-13 21:10:49 -0600

    updated crud methods

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,4 @@
-DROP TABLE IF EXISTS burgers_db;
+DROP DATABASE IF EXISTS burger_db;
 
-CREATE DATABASE IF NOT EXISTS burgers_db;
-USE burgers_db;
-
-
--- Create the burgers table
-CREATE TABLE burgers (
-    id int NOT NULL AUTO_INCREMENT,
-    burger_name varchar(255) NOT NULL,
-    devoured BOOL DEFAULT false,
-    PRIMARY KEY (id)
-);
+CREATE DATABASE burger_db;
+USE burger_db;
--- server.js
+++ server.js
@@ -1,26 +1,29 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 
+var db = require("./models");
+
 var PORT = process.env.PORT || 8000;
 var app = express();
 
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-// parse application/json
 app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burgersController.js");
+require("./controllers/burgersController.js")(app)
 
-app.use(routes);
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("Listening on port:%s", PORT);
+  });
+})
 
-app.listen(PORT, function() {
-  console.log("Listening on port:%s", PORT);
-});
